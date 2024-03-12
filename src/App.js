da4f56d45a2dceb2e2916e1d93469bb77ade2b53
shcher-olha <shcher.olha@gmail.com>
2024-03-12 17:42:33 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/shcher-olha"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Shcherbak Olha
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/shcher-olha/react-weather-week-5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
 
-export default App;
