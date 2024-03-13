c3bc049bfc273675b76b97afea4f87a2d3f71ea3
shcher-olha <shcher.olha@gmail.com>
2024-03-13 17:50:26 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/shcher-olha"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Shcherbak Olha
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/shcher-olha/react-weather-week-5"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/shcher-olha"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Shcherbak Olha
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/shcher-olha/react-weather-week-5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
